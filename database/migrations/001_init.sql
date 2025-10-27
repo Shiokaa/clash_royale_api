0af992979e79268e728091ee18f2209c332b71c9
Shioka <tom.amaru09@gmail.com>
2025-10-27 21:58:37 +0100

    fix(database): update migrations and seeds, add new tables

--- database/migrations/001_init.sql
+++ database/migrations/001_init.sql
@@ -1,50 +1,69 @@
 CREATE DATABASE IF NOT EXISTS clash_royale_api;
+
 USE clash_royale_api;
 
--- Table cards
-CREATE TABLE IF NOT EXISTS cards (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    description TEXT NOT NULL,
-    rarity ENUM('common', 'rare', 'epic', 'legendary', 'champion') NOT NULL,
-    type ENUM('spell', 'troop', 'building') NOT NULL,
-    unlocked_arena TINYINT NOT NULL,
-    elixir_cost TINYINT NOT NULL,
-    has_evolution BOOLEAN NOT NULL DEFAULT 0,
-    image_url VARCHAR(255)
-);
+-- Table des cartes
+-- Cette table contient tous les détails sur une carte dont ses statistiques
+CREATE TABLE
+    IF NOT EXISTS cards (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(25) NOT NULL,
+        description TEXT NOT NULL,
+        rarity ENUM ('common', 'rare', 'epic', 'legendary', 'champion') NOT NULL,
+        type ENUM ('spell', 'troop', 'building') NOT NULL,
+        unlocked_arena TINYINT NOT NULL,
+        elixir_cost TINYINT NOT NULL,
+        image_url VARCHAR(255),
+        stats JSON
+    );
 
--- Table card_stats
-CREATE TABLE IF NOT EXISTS card_stats (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    card_id INT NOT NULL UNIQUE,
-    hitpoints SMALLINT,
-    damage SMALLINT,
-    damage_second SMALLINT,
-    hit_speed DECIMAL(4,2),
-    speed ENUM('slow','medium','fast','very fast'),
-    count TINYINT,
-    `range` DECIMAL(4,2),
-    duration DECIMAL(4,2),
-    tower_damage SMALLINT,
-    tower_damage_second SMALLINT,
-    radius DECIMAL(4,2),
-    lifetime TINYINT,
-    target ENUM('air', 'ground', 'building', 'air & ground'),
-    death_damage SMALLINT,
-    shield SMALLINT,
-    damage_over_time JSON,
-    FOREIGN KEY (card_id) REFERENCES cards(id) ON DELETE CASCADE
-);
+-- Table des statistiques de chaque carte par leur niveau
+-- Cette table contient l'id de la carte en "FOREIGN KEY", le niveau ainsi que les statistiques en fonction de ce niveau
+CREATE TABLE
+    IF NOT EXISTS card_stats_by_levels (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        card_id INT NOT NULL,
+        level TINYINT NOT NULL,
+        stats JSON,
+        UNIQUE (card_id, level),
+        FOREIGN KEY (card_id) REFERENCES cards (id) ON DELETE CASCADE
+    );
 
+-- Table d'une capacité d'un champion
+-- Cette table contient l'id de la carte en "FOREIGN KEY", ainsi que les détails de la capacité
+CREATE TABLE
+    IF NOT EXISTS champion_abilities (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        card_id INT UNIQUE,
+        name VARCHAR(25) NOT NULL,
+        description TEXT NOT NULL,
+        elixir_cost TINYINT NOT NULL,
+        cooldown TINYINT NOT NULL,
+        image_url VARCHAR(255),
+        effect JSON,
+        FOREIGN KEY (card_id) REFERENCES cards (id) ON DELETE CASCADE
+    );
 
--- Table card_levels_stats
-CREATE TABLE IF NOT EXISTS card_levels_stats (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    card_id INT NOT NULL UNIQUE,
-    level TINYINT NOT NULL,
-    hitpoints SMALLINT,
-    damage SMALLINT,
-    damage_over_time JSON,
-    FOREIGN KEY (card_id) REFERENCES cards(id) ON DELETE CASCADE
-);
+    -- Table d'une carte en version évoluée
+    -- Cette contient l'id de la carte en "FOREIGN KEY", ainsi que les ajouts d'une carte évoluée
+CREATE TABLE
+    IF NOT EXISTS evolutions_cards (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        card_id INT UNIQUE,
+        cycle_cost TINYINT NOT NULL,
+        image_url VARCHAR(255),
+        evolution_stats JSON,
+        FOREIGN KEY (card_id) REFERENCES cards (id) ON DELETE CASCADE
+    );
+
+    -- Table des statistiques de chaque carte évoluée par leur niveau
+    -- Cette table contient l'id de la carte en "FOREIGN KEY", le niveau ainsi que les statistiques en fonction de ce niveau pour les cartes évoluées
+CREATE TABLE
+    IF NOT EXISTS evolution_card_stats_by_levels (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        card_id INT NOT NULL,
+        level TINYINT NOT NULL,
+        evolution_stats JSON,
+        UNIQUE (card_id, level),
+        FOREIGN KEY (card_id) REFERENCES cards (id) ON DELETE CASCADE
+    );
