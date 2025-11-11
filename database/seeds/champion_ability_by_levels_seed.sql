c6aedd1bff392b606bc8542c3fb018a82fcae58b
Shioka <tom.amaru09@gmail.com>
2025-11-11 16:17:57 +0100

    feature(seeds): new insert into champion_ability_by_levels_seed

---
+++ database/seeds/champion_ability_by_levels_seed.sql
@@ -0,0 +1,56 @@
+USE clash_royale_api;
+
+-- Insertions pour la table "champion_ability_by_levels"
+INSERT INTO
+    champion_ability_by_levels (champion_ability_id, level, stats)
+VALUES
+    (
+        1,
+        11,
+        '{"royal_rescue_damage:":256,"hitpoints":1.600,"damage":202,"dps":168}'
+    ),
+    (
+        1,
+        12,
+        '{"royal_rescue_damage:":281,"hitpoints":1.756,"damage":221,"dps":184}'
+    ),
+    (
+        1,
+        13,
+        '{"royal_rescue_damage:":309,"hitpoints":1.931,"damage":244,"dps":203}'
+    ),
+    (
+        1,
+        14,
+        '{"royal_rescue_damage:":339,"hitpoints":2.118,"damage":267,"dps":222}'
+    ),
+    (
+        1,
+        15,
+        '{"royal_rescue_damage:":372,"hitpoints":2.325,"damage":293,"dps":244}'
+    ),
+    (2, 11, '{"damage":335}'),
+    (2, 12, '{"damage":368}'),
+    (2, 13, '{"damage":404}'),
+    (2, 14, '{"damage":444}'),
+    (2, 15, '{"damage":487}'),
+    (3, 11, '{"damage":81,"dps":81}'),
+    (3, 12, '{"damage":89,"dps":89}'),
+    (3, 13, '{"damage":98,"dps":98}'),
+    (3, 14, '{"damage":108,"dps":108}'),
+    (3, 15, '{"damage":119,"dps":119}'),
+    (4, 11, '{"damage":332}'),
+    (4, 12, '{"damage":365}'),
+    (4, 13, '{"damage":401}'),
+    (4, 14, '{"damage":440}'),
+    (4, 15, '{"damage":483}'),
+    (5, 11, '{"dps":525}'),
+    (5, 12, '{"dps":576}'),
+    (5, 13, '{"dps":632}'),
+    (5, 14, '{"dps":695}'),
+    (5, 15, '{"dps":763}'),
+    (6, 11, '{"damage":107,"crown_tower_damage":23}'),
+    (6, 12, '{"damage":118,"crown_tower_damage":25}'),
+    (6, 13, '{"damage":129,"crown_tower_damage":27}'),
+    (6, 14, '{"damage":142,"crown_tower_damage":30}'),
+    (6, 15, '{"damage":156,"crown_tower_damage":33}'),
